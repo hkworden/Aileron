1c82b98182ff6a7a78a8fbd7c64df433fe712719
Hilary Worden <hkworden@gmail.com>
2011-08-13 17:59:42 -0400

    index page station selector

---
+++ aileron/static/js/main.js
@@ -0,0 +1,3 @@
+function main() {
+  $(".chzn-select").chosen();
+}
--- aileron/views/index.py
+++ aileron/views/index.py
@@ -1,10 +1,44 @@
+import string
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+from data.model.station import Station
+
 class IndexPage(webapp.RequestHandler):
+  
+  HTML_HEADER = (
+      '</html>'
+      '<head>'
+      '<title>Aileron</title>'
+      '<link rel="stylesheet" type="text/css" href="css/chosen.css"/>'
+      '<script src="js/jquery.min.js" type="text/javascript"></script>'
+      '<script src="js/chosen.jquery.min.js" type="text/javascript"></script>'
+      '<script src="js/main.js" type="text/javascript"></script>'
+      '</head>'
+      '<body onload="main();">'
+      )
+  
+  HTML_FOOTER = (
+      '</body>'
+      '</html>'
+      )
+  
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write('hello world')
+    self.response.headers['Content-Type'] = 'text/html'
+    write = self.response.out.write
+    write(IndexPage.HTML_HEADER)
+    self.write_station_options(write)
+    write(IndexPage.HTML_FOOTER)
+  
+  def write_station_options(self, write):
+    write('<select class="chzn-select" style="width:350px;">')
+    q = Station.all()
+    q.order('display_name')
+    for station in q:
+      write('<option>%s (%s)</option>' % (station.display_name, \
+          string.join(station.trains, '/')))
+    write('</select>')
 
 application = webapp.WSGIApplication([('/?', IndexPage)], debug=True)
 
